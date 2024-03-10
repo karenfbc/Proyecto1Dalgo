f34ca8a33cdcd1d9977787f74da9a48b19b82b23
karenfbc <k.fuentesb@uniandes.edu.co>
2024-03-10 13:45:24 -0500

    casos prueba

--- Prueba_6.py
+++ Prueba_6.py
@@ -44,3 +44,10 @@ print(min_movimientos([1, 2]))  # Expected: 1
 print(min_movimientos([3, 2, 1, 4]))  # Expected: 3
 print(min_movimientos([3, 2, 2, 4]))  # Expected: 2
 print(min_movimientos([7, 0, 0, 0, 0, 0, 0, 1]))  # Expected: 6
+print(min_movimientos([20, 32, 11, 7, 5, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))  # Expected: 1
+print(min_movimientos([18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 6, 6, 23, 29]))  # Expected: 536
+print(min_movimientos([24, 36, 38, 14, 7, 7, 7, 2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])) # Expected: 2
+print(min_movimientos([20, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 9, 3, 5, 14, 19, 23, 32])) # Expected: 857
+print(min_movimientos([4, 3, 2, 2, 4])) # Expected: 3
+print(min_movimientos([4, 3, 0, 1, 1])) # Expected: 1
+    
