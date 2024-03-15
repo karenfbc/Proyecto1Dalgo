8e96f28f0b21d3db2fb08910df221c069b41d1c1
AlisonAristizabal <a.aristizabalg@uniandes.edu.co>
2024-03-15 10:42:47 -0500

    prueba 10 funcionando

--- Prueba_10.py
+++ Prueba_10.py
@@ -15,21 +15,17 @@ def min_movimientos(torres):
 
         else:
             minMov = 10**9
-            if torres[pos] > torres[pos +1]:
-                    torres[pos + 1] += 1
-                    torres[pos] -= 1
-                    minMov = min(minMov, 1 + min_move_0(list(torres), pos, recorrido))
+            if torres[pos] > torres[pos +1] or torres[pos-1] > torres[pos +1]:
+                    if torres[pos-1] >= torres[pos]:
+                        torres[pos -1] -= 1
+                        torres[pos+1] += 1
+                        minMov= min(minMov,2 + min_move_0(torres, pos, recorrido))
+                    else:
+                        torres[pos + 1] += 1
+                        torres[pos] -= 1
+                        minMov = min(minMov, 1 + min_move_0(list(torres), pos, recorrido))
             else:
-                if torres[pos] > torres[pos -1] and torres[pos-1] <= torres[pos + 1]:
-                    torres[pos ] -= 1
-                    torres[pos-1] += 1
-                    minMov = min(minMov, 1 + min_move_0(list(torres), pos, recorrido))
-                elif torres[pos] > torres[pos -1]:
-                    torres[pos ] -= 1
-                    torres[pos-1] += 1
-                    minMov = min(minMov, 1 + min_move_0(list(torres), pos-1, recorrido))
-                else:
-                    minMov = min(minMov, min_move_0(list(torres), pos - 1, recorrido))
+                minMov = min(minMov, min_move_0(list(torres), pos-1, recorrido))
 
             matriz[recorrido][pos] = minMov
         return matriz[recorrido][pos] 
@@ -40,4 +36,18 @@ def min_movimientos(torres):
     else:
         return min_move_0(torres, n - 1, recorrido)
 
-print(min_movimientos([1, 0, 0]))
+"""print(min_movimientos([1, 1, 0, 0]))
+print(min_movimientos([0]))  # Expected: 0
+print(min_movimientos([1]))  # Expected: 0
+print(min_movimientos([1, 1]))  # Expected: 0
+print(min_movimientos([1, 2]))  # Expected: 1
+print(min_movimientos([2, 1, 4]))  # Expected: 3
+print(min_movimientos([2, 2, 4]))  # Expected: 2
+print(min_movimientos([0, 0, 0, 0, 0, 0, 1]))  # Expected: 6
+print(min_movimientos([32, 11, 7, 5, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))  # Expected: 1
+print(min_movimientos([18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 6, 6, 23, 29]))  # Expected: 536
+print(min_movimientos([24, 36, 38, 14, 7, 7, 7, 2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])) # Expected: 2
+print(min_movimientos([20, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 9, 3, 5, 14, 19, 23, 32])) # Expected: 857
+print(min_movimientos([4, 3, 2, 2, 4])) # Expected: 3
+print(min_movimientos([4, 3, 0, 1, 1])) # Expected: 1"""
+print(min_movimientos([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 5 ,7, 11, 32])) # expected:1
